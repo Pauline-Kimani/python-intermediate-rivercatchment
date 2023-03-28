3b67d3c5418a895378289e72d1790423829cff38
Pauline-Kimani <kimanipauline.pnk@gmail.com>
2023-03-28 15:49:16 +0100

    Add parameterisation mean case

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import pandas.testing as pdt
 import datetime
+import pytest as pytest
 
 
 def test_daily_mean_zeros():
@@ -10,18 +11,18 @@ def test_daily_mean_zeros():
     from catchment.models import daily_mean
 
     test_input = pd.DataFrame(
-                     data=[[0.0, 0.0],
-                           [0.0, 0.0],
-                           [0.0, 0.0]],
-                     index=[pd.to_datetime('2000-01-01 01:00'),
-                            pd.to_datetime('2000-01-01 02:00'),
-                            pd.to_datetime('2000-01-01 03:00')],
-                     columns=['A', 'B']
+        data=[[0.0, 0.0],
+              [0.0, 0.0],
+              [0.0, 0.0]],
+        index=[pd.to_datetime('2000-01-01 01:00'),
+               pd.to_datetime('2000-01-01 02:00'),
+               pd.to_datetime('2000-01-01 03:00')],
+        columns=['A', 'B']
     )
     test_result = pd.DataFrame(
-                     data=[[0.0, 0.0]],
-                     index=[datetime.date(2000, 1, 1)],
-                     columns=['A', 'B']
+        data=[[0.0, 0.0]],
+        index=[datetime.date(2000, 1, 1)],
+        columns=['A', 'B']
     )
 
     # Need to use Pandas testing functions to compare arrays
@@ -33,18 +34,18 @@ def test_daily_mean_integers():
     from catchment.models import daily_mean
 
     test_input = pd.DataFrame(
-                     data=[[1, 2],
-                           [3, 4],
-                           [5, 6]],
-                     index=[pd.to_datetime('2000-01-01 01:00'),
-                            pd.to_datetime('2000-01-01 02:00'),
-                            pd.to_datetime('2000-01-01 03:00')],
-                     columns=['A', 'B']
+        data=[[1, 2],
+              [3, 4],
+              [5, 6]],
+        index=[pd.to_datetime('2000-01-01 01:00'),
+               pd.to_datetime('2000-01-01 02:00'),
+               pd.to_datetime('2000-01-01 03:00')],
+        columns=['A', 'B']
     )
     test_result = pd.DataFrame(
-                     data=[[3.0, 4.0]],
-                     index=[datetime.date(2000, 1, 1)],
-                     columns=['A', 'B']
+        data=[[3.0, 4.0]],
+        index=[datetime.date(2000, 1, 1)],
+        columns=['A', 'B']
     )
 
     # Need to use Pandas testing functions to compare arrays
@@ -60,8 +61,8 @@ def test_daily_max_integers():
               [6, 8],
               [10, 12]],
         index=[pd.to_datetime('2005-12-01 23:00'),
-                   pd.to_datetime('2005-12-01 23:15'),
-                   pd.to_datetime('2005-12-01 23:30')],
+               pd.to_datetime('2005-12-01 23:15'),
+               pd.to_datetime('2005-12-01 23:30')],
         columns=['C', 'D']
     )
 
@@ -92,3 +93,39 @@ def test_daily_min_integers():
         index=[datetime.date(2005, 12, 1)],
         columns=['C', 'D']
     )
+
+
+# test parameterization to use the test code but with different data
+@pytest.mark.parametrize(
+    "test_data, test_index, test_columns, expected_data, expected_index, expected_columns",
+    [
+        (
+                [[0.0, 0.0], [0.0, 0.0], [0.0, 0.0]],
+                [pd.to_datetime('2000-01-01 01:00'),
+                 pd.to_datetime('2000-01-01 02:00'),
+                 pd.to_datetime('2000-01-01 03:00')],
+                ['A', 'B'],
+                [[0.0, 0.0]],
+                [datetime.date(2000, 1, 1)],
+                ['A', 'B']
+        ),
+        (
+                [[1, 2], [3, 4], [5, 6]],
+                [pd.to_datetime('2000-01-01 01:00'),
+                 pd.to_datetime('2000-01-01 02:00'),
+                 pd.to_datetime('2000-01-01 03:00')],
+                ['A', 'B'],
+                [[3.0, 4.0]],
+                [datetime.date(2000, 1, 1)],
+                ['A', 'B']
+        ),
+    ])
+
+def test_daily_mean(test_data, test_index, test_columns,
+                    expected_data, expected_index, expected_columns):
+    """Test mean function works with zeros and positive integers"""
+    from catchment.models import daily_mean
+    pdt.assert_frame_equal(
+        daily_mean(pd.DataFrame(data=test_data, index=test_index, columns=test_columns)),
+        pd.DataFrame(data=expected_data, index=expected_index, columns=expected_columns))
+
