74b1ce4f5f6afc9678628ad31d331d556b710131
Pauline-Kimani <kimanipauline.pnk@gmail.com>
2023-03-28 15:04:47 +0100

    Add min, max test case

--- tests/test_models.py
+++ tests/test_models.py
@@ -49,3 +49,46 @@ def test_daily_mean_integers():
 
     # Need to use Pandas testing functions to compare arrays
     pdt.assert_frame_equal(daily_mean(test_input), test_result)
+
+
+def test_daily_max_integers():
+    """"Test max function is working for an array of positive integers"""
+    from catchment.models import daily_max
+
+    test_input = pd.DataFrame(
+        data=[[2, 4],
+              [6, 8],
+              [10, 12]],
+        index=[pd.to_datetime('2005-12-01 23:00'),
+                   pd.to_datetime('2005-12-01 23:15'),
+                   pd.to_datetime('2005-12-01 23:30')],
+        columns=['C', 'D']
+    )
+
+    test_result = pd.DataFrame(
+        data=[[6, 8]],
+        index=[datetime.date(2005, 12, 1)],
+        columns=['C', 'D']
+    )
+
+
+def test_daily_min_integers():
+    """"Test min function is working for an array of positive and negative integers"""
+
+    from catchment.models import daily_min
+
+    test_input = pd.DataFrame(
+        data=[[1, -3],
+              [5, -7],
+              [-9, 11]],
+        index=[pd.to_datetime('2005-12-01 23:00'),
+               pd.to_datetime('2005-12-01 23:15'),
+               pd.to_datetime('2005-12-01 23:30')],
+        columns=['C', 'D']
+    )
+
+    test_result = pd.DataFrame(
+        data=[[5, -7]],
+        index=[datetime.date(2005, 12, 1)],
+        columns=['C', 'D']
+    )
